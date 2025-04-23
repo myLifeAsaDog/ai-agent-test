2fd4976624b5fed1b2d8658eddd1c572ad9fdfd7
k-miyoshi <miyoshiez22@gmail.com>
2025-04-23 21:54:46 +0900

    add prerendered

---
+++ docs/server/chunks/0-CLqAbLqC.js
@@ -0,0 +1,17 @@
+const prerender = true;
+
+var _layout_ts = /*#__PURE__*/Object.freeze({
+  __proto__: null,
+  prerender: prerender
+});
+
+const index = 0;
+let component_cache;
+const component = async () => component_cache ??= (await import('./layout.svelte-4V-_rMCU.js')).default;
+const universal_id = "src/routes/+layout.ts";
+const imports = ["_app/immutable/nodes/0.Cy394Tda.js","_app/immutable/chunks/JmO34ccS.js","_app/immutable/chunks/XrXDYhV9.js"];
+const stylesheets = [];
+const fonts = [];
+
+export { component, fonts, imports, index, stylesheets, _layout_ts as universal, universal_id };
+//# sourceMappingURL=0-CLqAbLqC.js.map
---
+++ docs/server/manifest.js
@@ -0,0 +1,42 @@
+const manifest = (() => {
+function __memo(fn) {
+	let value;
+	return () => value ??= (value = fn());
+}
+
+return {
+	appDir: "_app",
+	appPath: "_app",
+	assets: new Set(["favicon.png"]),
+	mimeTypes: {".png":"image/png"},
+	_: {
+		client: {start:"_app/immutable/entry/start.D5DfQNY8.js",app:"_app/immutable/entry/app.ByQ8T7-b.js",imports:["_app/immutable/entry/start.D5DfQNY8.js","_app/immutable/chunks/KDWdzRBo.js","_app/immutable/chunks/XrXDYhV9.js","_app/immutable/chunks/CVAQYCPX.js","_app/immutable/entry/app.ByQ8T7-b.js","_app/immutable/chunks/XrXDYhV9.js","_app/immutable/chunks/DYMbm4s-.js","_app/immutable/chunks/JmO34ccS.js","_app/immutable/chunks/DA3f3Alq.js","_app/immutable/chunks/CVAQYCPX.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		nodes: [
+			__memo(() => import('./chunks/0-CLqAbLqC.js')),
+			__memo(() => import('./chunks/1-Bf5J4ERP.js'))
+		],
+		routes: [
+			{
+				id: "/api/chat",
+				pattern: /^\/api\/chat\/?$/,
+				params: [],
+				page: null,
+				endpoint: __memo(() => import('./chunks/_server.ts-wT1r-yX6.js'))
+			}
+		],
+		prerendered_routes: new Set(["/"]),
+		matchers: async () => {
+			
+			return {  };
+		},
+		server_assets: {}
+	}
+}
+})();
+
+const prerendered = new Set(["/"]);
+
+const base = "";
+
+export { base, manifest, prerendered };
+//# sourceMappingURL=manifest.js.map
