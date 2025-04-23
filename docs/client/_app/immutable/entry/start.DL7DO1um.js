f5e4f5bf5bc1a332ac9172c71c550faa61a0da97
k-miyoshi <miyoshiez22@gmail.com>
2025-04-23 21:36:36 +0900

    Change directory to docs from build

--- docs/client/_app/immutable/entry/start.DL7DO1um.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/BvK8wjA0.js";export{o as load_css,r as start};
--- svelte.config.js
+++ svelte.config.js
@@ -12,9 +12,13 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-      out: 'docs',
-      fallback: 'index.html'
-    }),
+			assets: './docs',
+			pages: './docs',
+			fallback: 'index.html'
+		}),
+		paths: {
+			base: '/ai-agent-test'
+		}
 	}
 };
 
