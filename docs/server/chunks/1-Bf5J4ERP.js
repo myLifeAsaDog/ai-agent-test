ba4fd231cbec791574268bafa4483a8227e3bc39
k-miyoshi <miyoshiez22@gmail.com>
2025-04-23 22:08:03 +0900

    remove docs

--- docs/server/chunks/1-Bf5J4ERP.js
+++
@@ -1,9 +0,0 @@
-const index = 1;
-let component_cache;
-const component = async () => component_cache ??= (await import('./error.svelte-CkoB8NEC.js')).default;
-const imports = ["_app/immutable/nodes/1.D28jvfM6.js","_app/immutable/chunks/JmO34ccS.js","_app/immutable/chunks/XrXDYhV9.js","_app/immutable/chunks/B0YgV7DX.js","_app/immutable/chunks/DYMbm4s-.js","_app/immutable/chunks/KDWdzRBo.js","_app/immutable/chunks/CVAQYCPX.js"];
-const stylesheets = [];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=1-Bf5J4ERP.js.map
